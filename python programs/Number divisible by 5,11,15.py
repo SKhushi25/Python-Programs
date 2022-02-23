1492250cdaa0bfc370786346658e2f399afc463f
SKhushi25 <94968686+SKhushi25@users.noreply.github.com>
2022-02-23 21:27:57 +0530

    Add files via upload

---
+++ python programs/Number divisible by 5,11,15.py
@@ -0,0 +1,8 @@
+num=int(input(" Enter any Number : "))
+
+if num%5==0:
+    print(" It is Divisible by 5.")
+if num%11==0:
+    print(" It is Divisible by 11.")
+if num%15==0:
+    print(" It is Divisible by 15.")
